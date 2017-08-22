8ca1f84df65378e85788ccf014b4f8836164250d
Sébastien Saunier <seb@saunier.me>
2017-08-22 14:55:37 +0200

    Remove jQuery

--- app/assets/javascripts/google_maps_autocomplete.js
+++ app/assets/javascripts/google_maps_autocomplete.js
@@ -1,26 +1,17 @@
-$(document).ready(function() {
-  var flat_address = $('#flat_address').get(0);
-
-  if (flat_address) {
-    var autocomplete = new google.maps.places.Autocomplete(flat_address, { types: ['geocode'] });
-    google.maps.event.addListener(autocomplete, 'place_changed', onPlaceChanged);
-    google.maps.event.addDomListener(flat_address, 'keydown', function(e) {
-      if (e.keyCode == 13) {
-        e.preventDefault(); // Do not submit the form on Enter.
-      }
-    });
-  }
-});
-
 function onPlaceChanged() {
   var place = this.getPlace();
   var components = getAddressComponents(place);
 
-  $('#flat_address').trigger('blur').val(components.address);
-  $('#flat_zip_code').val(components.zip_code);
-  $('#flat_city').val(components.city);
+  var flatAddress = document.getElementById('flat_address');
+  flatAddress.blur();
+  flatAddress.value = components.address;
+
+  document.getElementById('flat_zip_code').value = components.zip_code;
+  document.getElementById('flat_city').value = components.city;
+
   if (components.country_code) {
-    $('#flat_country').val(components.country_code);
+    var selector = '#flat_country option[value="' + components.country_code + '"]';
+    document.querySelector(selector).selected = true;
   }
 }
 
@@ -28,6 +19,10 @@ function getAddressComponents(place) {
   // If you want lat/lng, you can look at:
   // - place.geometry.location.lat()
   // - place.geometry.location.lng()
+
+  if (window.console && typeof console.log === "function") {
+    console.log(place);
+  }
 
   var street_number = null;
   var route = null;
@@ -38,24 +33,40 @@ function getAddressComponents(place) {
     var component = place.address_components[i];
     for (var j in component.types) {
       var type = component.types[j];
-      if (type == 'street_number') {
+      if (type === 'street_number') {
         street_number = component.long_name;
-      } else if (type == 'route') {
+      } else if (type === 'route') {
         route = component.long_name;
-      } else if (type == 'postal_code') {
+      } else if (type === 'postal_code') {
         zip_code = component.long_name;
-      } else if (type == 'locality') {
+      } else if (type === 'locality') {
         city = component.long_name;
-      } else if (type == 'country') {
+      } else if (type === 'postal_town' && city === null) {
+        city = component.long_name;
+      } else if (type === 'country') {
         country_code = component.short_name;
       }
     }
   }
 
   return {
-    address: street_number == null ? route : (street_number + ' ' + route),
+    address: street_number === null ? route : (street_number + ' ' + route),
     zip_code: zip_code,
     city: city,
     country_code: country_code
   };
 }
+
+document.addEventListener("DOMContentLoaded", function() {
+  var flatAddress = document.getElementById('flat_address');
+
+  if (flatAddress) {
+    var autocompvare = new google.maps.places.Autocompvare(flatAddress, { types: ['geocode'] });
+    google.maps.event.addListener(autocompvare, 'place_changed', onPlaceChanged);
+    google.maps.event.addDomListener(flatAddress, 'keydown', function(e) {
+      if (e.key === "Enter") {
+        e.preventDefault(); // Do not submit the form on Enter.
+      }
+    });
+  }
+});
